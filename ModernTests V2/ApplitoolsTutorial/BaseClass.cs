e952e344ef4a331b1d46558f5cb33e718890add6
Shyam Bhattarai <shyam.bhattarai@verscend.com>
2020-07-08 13:18:39 +0545

    Add missing files

---
+++ ModernTests V2/ApplitoolsTutorial/BaseClass.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Applitools;
+using Applitools.Selenium;
+using Applitools.VisualGrid;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Configuration = Applitools.Selenium.Configuration;
+using ScreenOrientation = Applitools.VisualGrid.ScreenOrientation;
+
+namespace ApplitoolsTutorial
+{
+    public class BaseClass
+    {
+        public static BatchInfo batch { get; set; }
+        public IWebDriver webDriver { get; set; }
+        public Eyes eyes { get; set; }
+        public VisualGridRunner runner { get; set; }
+        public Configuration config { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            webDriver = new ChromeDriver();
+            runner = new VisualGridRunner(10);
+            config = new Configuration();
+            
+            eyes = new Eyes(runner)
+            {
+                ApiKey = Environment.GetEnvironmentVariable("APPLITOOLS_API_KEY", EnvironmentVariableTarget.User)
+            };
+
+            config.SetBatch(new BatchInfo("UFG Hackathon"));
+
+            // Adding browsers with different viewports
+            config.AddBrowser(1200, 700, BrowserType.CHROME);
+            config.AddBrowser(1200, 700, BrowserType.FIREFOX);
+            config.AddBrowser(1200, 700, BrowserType.EDGE_CHROMIUM);
+            config.AddBrowser(768, 700, BrowserType.CHROME);
+            config.AddBrowser(768, 700, BrowserType.FIREFOX);
+            config.AddBrowser(768, 700, BrowserType.EDGE_CHROMIUM);
+
+            // Adding mobile emulation devices in Portrait mode
+            config.AddDeviceEmulation(DeviceName.iPhone_X, ScreenOrientation.Portrait);
+            config.AddDeviceEmulation(DeviceName.Pixel_2, ScreenOrientation.Portrait);
+
+            // Setting the configuration object to eyes
+            eyes.SetConfiguration(config);
+        }
+
+        [TearDown]
+        public void TearDownForEverySingleTestMethod()
+        {
+            //Close your Selenium browser
+            webDriver.Quit();
+            //Close applitools eyes so that your test run is saved
+            eyes.Close();
+            //Quit applitools if it is not already closed
+            eyes.AbortIfNotClosed();
+        }
+
+        public void GoToApplication()
+        {
+            //This uses Selenium to navigate to a url of the page below
+            webDriver.Navigate().GoToUrl("https://demo.applitools.com/gridHackathonV1.html");
+        }
+
+        public bool IsElementPresentByCssOrXpath( string locator, bool cssSelector = true)
+        {
+            bool elementPresent = true;
+
+            try
+            {
+                var element = (cssSelector) ? webDriver.FindElement(By.CssSelector(locator)) : 
+                    webDriver.FindElement(By.XPath(locator));
+            }
+            catch (Exception e)
+            {
+                elementPresent = false;
+            }
+
+            return elementPresent;
+        }
+    }
+}
---
+++ ModernTests V2/ApplitoolsTutorial/UFGDemo.cs
@@ -0,0 +1,101 @@
+﻿using Applitools;
+using Applitools.Selenium;
+using System;
+using System.Drawing;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace ApplitoolsTutorial
+{
+    public class UFGDemo: BaseClass
+    {
+        [Test]
+		public void Task1()
+		{
+            try
+            
+            {
+                GoToApplication();
+				eyes.Open(webDriver, "Demo App", "Cross-Device Elements Test", new Size(800, 600));
+                eyes.Check("Cross-Device Elements Test", Target.Window().Fully().WithName("Main Shopping Page"));
+
+				// Call Close on eyes to let the server know it should display the results
+				eyes.CloseAsync();
+
+			}
+			catch (Exception e)
+			{
+				eyes.AbortAsync();
+			}
+
+        }
+
+
+        [Test]
+        public void Task2()
+        {
+            try
+
+            {
+                GoToApplication();
+                // Call Open on eyes to initialize a test session
+                eyes.Open(webDriver, "Demo App", "Filter Results", new Size(800, 600));
+
+                webDriver.FindElement(By.Id("ti-filter")).Click();
+                
+                if(IsElementPresentByCssOrXpath("//label[text()=\"Black \"]/span[contains(@class,'checkmark')]", false))
+                    webDriver.FindElement(By.XPath("//label[text()=\"Black \"]/span[contains(@class,'checkmark')]")).Click();
+
+                else
+                {
+                    webDriver.FindElement(By.Id("ti-filter")).Click();
+                }
+                webDriver.FindElement(By.Id("filterBtn")).Click();
+
+                eyes.Check("Filter Results", Target.Region(By.Id("product_grid")));
+
+                // Call Close on eyes to let the server know it should display the results
+                eyes.CloseAsync();
+            }
+            catch (Exception e)
+            {
+                eyes.AbortAsync();
+            }
+        }
+
+        [Test]
+        public void Task3()
+        {
+            try
+
+            {
+                GoToApplication();
+                // Call Open on eyes to initialize a test session
+                eyes.Open(webDriver, "Demo App", "Product Details test", new Size(800, 600));
+
+                webDriver.FindElement(By.Id("ti-filter")).Click();
+
+                if (IsElementPresentByCssOrXpath("//label[text()=\"Black \"]/span[contains(@class,'checkmark')]", false))
+                    webDriver.FindElement(By.XPath("//label[text()=\"Black \"]/span[contains(@class,'checkmark')]")).Click();
+
+                else
+                {
+                    webDriver.FindElement(By.Id("ti-filter")).Click();
+                }
+                webDriver.FindElement(By.Id("filterBtn")).Click();
+
+                webDriver.FindElement(By.Id("product_1")).Click();
+
+                eyes.Check("Product Details test", Target.Window().Fully().WithName("Main Shopping Page"));
+
+                // Call Close on eyes to let the server know it should display the results
+                eyes.CloseAsync();
+            }
+            catch (Exception e)
+            {
+                eyes.AbortAsync();
+            }
+
+        }
+    }
+}
